b5d360faa3cde53c9a507091767b1e4d51922cb8
IIVader <47644831+IIVader@users.noreply.github.com>
2024-08-28 19:43:03 -0300

    Subindo testes, facade e a exception de usuario não encontrado

---
+++ myfood/src/Main.java
@@ -0,0 +1,10 @@
+import easyaccept.EasyAccept;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] args2 = {"br.ufal.ic.p2.myfood.Facade",
+                "tests/us1_1.txt",
+        };
+        EasyAccept.main(args2);
+    }
+}
---
+++ myfood/src/br/ufal/ic/p2/myfood/Facade.java
@@ -0,0 +1,9 @@
+package br.ufal.ic.p2.myfood;
+
+import easyaccept.EasyAccept;
+
+public class Facade {
+
+    public void zerarSistema (){}
+
+}
